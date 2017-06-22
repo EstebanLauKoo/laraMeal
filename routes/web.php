268039613b776a14d86303e9a96cfcdfbf1e5f54
Esteban lau koo <Esteban lau koo>
2017-06-22 16:54:44 -0400

    working on logOut

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', 'WelcomeController@index');
+
+Route::get('/', 'WelcomeController@index');
+
+Route::auth();
+
+Route::get('/logout', 'LoginController@logout');
+
+Route::get('/home', 'HomeController@index');
+
+Route::post('/users/{user}/meals', 'MealsController@store');
+
+Route::resource('/meals', 'MealsController');
+
+Route::post('/meals/{meal}/foods', 'FoodsController@store');
+
